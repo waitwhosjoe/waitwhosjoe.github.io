6d7906c1c9b15164a3eefa0a76ca65bffb212d9e
waitwhosjoe <98497237+waitwhosjoe@users.noreply.github.com>
2022-04-16 17:28:34 -0500

    Delete games/Wordle directory

--- games/Wordle/src/main.ts
+++
@@ -1,9 +0,0 @@
-//! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
-import App from "./App.svelte";
-
-export default new App({
-	target: document.body,
-	props: {
-		version: "1.2.3",
-	}
-});
